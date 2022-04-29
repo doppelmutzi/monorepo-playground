eb03a750aec1bd8d5f71a1aade97ae8df6b2116f
dajiahuo <xzp6612@163.com>
2022-04-29 15:06:42 +0800

    feat: lib - text change

--- projects/lib/src/App.vue
+++ projects/lib/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App - test" />
     <AwesomeButton text="Hello World"></AwesomeButton>
   </div>
 </template>
@@ -14,8 +14,8 @@ export default {
   name: "app",
   components: {
     HelloWorld,
-    AwesomeButton
-  }
+    AwesomeButton,
+  },
 };
 </script>
 
